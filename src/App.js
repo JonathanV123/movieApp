4f51bea185119345eab10b4d766c1c4481922e36
Jonathan <jvoxland@gmail.com>
2017-04-18 13:47:53 -0700

    test commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header.js';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            movieData:[]
+        };
+    }
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <h1>Testing</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div id="testing">
+                <p>Hello World</p>
+            </div>
+        )
+    }
+}
