518472b1a5bc41a79eeffb72c7f6998dad304168
Jonathan <jvoxland@gmail.com>
2017-05-23 14:03:03 -0700

    moviePosters now show up in calendar

--- src/components/RenderIndividualDay.js
+++ src/components/RenderIndividualDay.js
@@ -3,18 +3,35 @@ import React from 'react';
 export default class RenderIndividualDay extends React.Component {
     constructor() {
         super();
-        this.renderTitle.bind(this);
+        // this.renderTitle.bind(this);
+        this.renderPoster.bind(this);
     }
-    renderTitle(){
+    // renderTitle(){
+    //     if(this.props.movieData.length > 0){
+    //         console.log(this.props.movieData[0].poster_path);
+    //         return this.props.movieData[0].title;
+    //     }
+    // }
+    renderPoster(){
         if(this.props.movieData.length > 0){
-            return this.props.movieData[0].title;
+            return this.props.movieData[0].poster_path
         }
     }
     render() {
         return (
-            <div className="individualDay">
+            <div style={{
+                width: "154px",
+                height: "100px",
+                border: "1px solid black",
+                display: "flex",
+                color:"white",
+                backgroundColor:"#303030",
+                alignItems:"flex-start",
+                backgroundRepeat:"no-repeat",
+                backgroundSize:"154px 100px",
+                backgroundImage: "url(" + "https://image.tmdb.org/t/p/w154" + this.renderPoster() + ")"}}>
                 {this.props.date.getDate()}
-                {this.renderTitle()}
+                {/*{this.renderTitle()}*/}
             </div>
         )
     }
